e92ee4077c57dc5082ac9149a37c1c6bb8476f75
DLucxoket <316610961@qq.com>
2026-03-14 23:56:49 +0800

    e

--- src/main/kotlin/org/dlucxoket/lucchatbot/LucChatBotApplication.kt
+++ src/main/kotlin/org/dlucxoket/lucchatbot/LucChatBotApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class LucChatBotApplication {
-    fun main(args: Array<String>) {
-        runApplication<LucChatBotApplication>(*args)
-    }
+class LucChatBotApplication
+
+fun main(args: Array<String>) {
+    runApplication<LucChatBotApplication>(*args)
 }
