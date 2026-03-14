f457560882b7cf5de211cfb437eb265d14815894
DLucxoket <316610961@qq.com>
2026-03-14 23:40:59 +0800

    初始化模块

---
+++ src/main/kotlin/org/dlucxoket/lucchatbot/LucChatBotApplication.kt
@@ -0,0 +1,11 @@
+package org.dlucxoket.lucchatbot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LucChatBotApplication {
+    fun main(args: Array<String>) {
+        runApplication<LucChatBotApplication>(*args)
+    }
+}
---
+++ src/test/kotlin/org/dlucxoket/lucchatbot/LucChatBotApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.dlucxoket.lucchatbot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LucChatBotApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
